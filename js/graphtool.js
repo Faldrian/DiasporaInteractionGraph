dff1b9d09ca59cbd16cfc344228fe91be78b74bb
Faldrian <faldrian@och-noe.de>
2015-01-11 02:19:53 +0100

    first working version with basic plugin

--- js/graphtool.js
+++ js/graphtool.js
@@ -1,14 +1,67 @@
 /**
  * Graph-Tool (since it will show some Graphs...)
- * 
+ *
  * - Load interactions from diaspora-post
  * - Allow multiple analyst-plugins to run after data is loaded
  * - Render each plugin to it's own space on the page
  */
 
+// add Plugins to this array.
+var analysePlugins = [];
+
+// define basic Plugin
+analysePlugins.push({
+  title: "Basic Numbers",
+
+  run: function (interactions, targetElement) {
+    targetElement.html(
+      ' Likes: '+interactions.likes.length+
+      ' Reshares: '+interactions.reshares.length+
+      ' Comments: '+interactions.comments.length);
+  }
+});
+
+
+function loadInteractions(posturl) {
+  // extract host and guid from url
+  var regexp = /https:\/\/(.*?)\/posts\/(.*)/;
+  var parts = posturl.match(regexp);
+
+  // check if we found all parts
+  if(parts.length != 3) {
+    alert("Url not valid.");
+    return;
+  }
+
+  // load interactions via proxy
+  $.getJSON('proxy.php', {
+    host: parts[1],
+    guid: parts[2]
+  })
+    .done(function(data) {
+      runPlugins(data);
+    })
+    .fail(function() {
+      alert("Request failed. Sorry.");
+    });
+}
+
+function runPlugins(interactions) {
+  analysePlugins.forEach(function(plugin) {
+    // create target element
+    var elem = $('<div class="pluginContainer"><h3>'+plugin.title+'</h3><div class="content"></div></div>');
+
+    // execute plugin
+    plugin.run(interactions, elem.find('.content'));
+
+    $('#pluginArea').append(elem);
+  });
+}
+
 
 $(function(){
-  $('button').click(function(){
-    alert("Geduld, alter!");
+  // Register handlers
+  $('#startprocess').click(function(){
+    loadInteractions($('#posturl').val());
   });
 });
--- proxy.php
+++ proxy.php
@@ -6,22 +6,21 @@
  */
 
 if(isset($_GET['host']) && isset($_GET['guid'])) {
+  header('content-type', 'application/json; charset=utf-8');
+
   // Construct interactions-url
   $host = $_GET['host'];
   $guid = $_GET['guid'];
   $url = "https://$host/posts/$guid/interactions.json";
-  
+
   // Load url
   $ch = curl_init();
 
   curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
   curl_setopt($ch, CURLOPT_URL, $url);
 
-  $content = curl_exec($ch);
+  curl_exec($ch);
   curl_close($ch);
-  
-  echo $content;
 }
-
 ?>
